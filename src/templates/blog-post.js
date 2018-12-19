3a3147de140b17c5b892f94001e702f3f5fcd27f
Lenz Gschwendtner <lenz@springtimesoft.com>
2018-12-19 23:41:26 +0000

    working on header image posts

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,12 +1,25 @@
 import React from "react"
 import { graphql } from "gatsby"
+import BlogHeader from '../pages/blog-header'
 
 export default ({ data }) => {
   const post = data.markdownRemark
   return (
-    <article className="cf ph3 ph5-ns pv5">
-      <h1>{post.frontmatter.title}</h1>
-      <div dangerouslySetInnerHTML={{ __html: post.html }} />
+    <article className="pb5">
+      <header className="vh-100 dt w-100">
+        <BlogHeader file={post.frontmatter.headerImage} className="dtc v-mid cover ph3 ph4-m ph5-l">
+          <h1 className="f2 f-subheadline-l measure lh-title fw9">{post.frontmatter.title}</h1>
+          <h2 className="f4 fw6">by {post.frontmatter.author}</h2>
+        </BlogHeader>
+      </header>
+      <div className="f4">
+        <a href="/blog">back</a>
+      </div>
+      <div className="ph3 ph4-m ph5-l">
+        <div className="lh-copy f5 f3-m f1-l measure center pv4">
+          <div dangerouslySetInnerHTML={{ __html: post.html }} />
+        </div>
+      </div>
     </article>
   )
 }
@@ -17,6 +30,9 @@ export const query = graphql`
       html
       frontmatter {
         title
+        author
+        headerImage
       }
     }
   }`
+
